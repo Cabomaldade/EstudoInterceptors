2bc10f22bc7fea3fad4ddabe10e17663addfe7c6
Cabomaldade <soutom@yahoo.com.br>
2019-06-15 16:01:07 -0300

    Terminado Estudo com Interceptors

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { HttpRequest, HttpXhrBackend } from '@angular/common/http';
+
+import { HttpInterceptorHandler } from './interceptor-handler';
+import { I1, I2 } from './interceptors';
+import { Observable } from 'rxjs';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+// tslint:disable-next-line: component-selector
+    selector: 'my-app',
+    template: `
+        <div><h3>Response</h3>{{response | async | json}}</div>
+        <button (click)="request()">Make request</button>`
+    ,
 })
 export class AppComponent {
-  title = 'EstudoInterceptors';
+    response: Observable<any>;
+    constructor(private backend: HttpXhrBackend) {}
+
+    request() {
+        const req = new HttpRequest('GET', 'https://jsonplaceholder.typicode.com/posts/1');
+        const i1Handler = new HttpInterceptorHandler(this.backend, new I1());
+        const i2Handler = new HttpInterceptorHandler(i1Handler, new I2());
+        this.response = i2Handler.handle(req);
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { HTTP_INTERCEPTORS, HttpBackend, HttpClientModule, HttpRequest, HttpXhrBackend } from '@angular/common/http';
+import { I1, I2 } from './interceptors';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    imports: [BrowserModule, HttpClientModule],
+    declarations: [
+        AppComponent
+    ],
+    providers: [
+        {
+            provide: HTTP_INTERCEPTORS,
+            useClass: I1,
+            multi: true
+        },
+        {
+            provide: HTTP_INTERCEPTORS,
+            useClass: I2,
+            multi: true
+        }
+    ],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
